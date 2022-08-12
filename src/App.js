44135960803ea66b472cf79cf064a2f18e344d0a
SM0413 <kbjtmdals@naver.com>
2022-08-12 12:51:07 +0900

    styled적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import styled from "styled-components";
+
+const Fater = styled.div`
+  display: flex;
+`;
+
+const Box1 = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+const Box2 = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
+
 function App() {
-  return null;
+  return (
+    <Fater>
+      <Box1>
+        <Text>Hello</Text>
+      </Box1>
+      <Box2 />
+    </Fater>
+  );
 }
 
 export default App;
