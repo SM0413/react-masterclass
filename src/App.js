bce0162373f965a23ab82e35b159bf386bea190a
SM0413 <kbjtmdals@naver.com>
2022-08-12 13:37:24 +0900

    Pseudo Selectors Part Two

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import styled, { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
 `;
 
 const rotationAnimation = keyframes`
@@ -18,6 +22,10 @@ const rotationAnimation = keyframes`
   }
 `;
 
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+
 const Box = styled.div`
   height: 200px;
   width: 200px;
@@ -26,21 +34,18 @@ const Box = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  span {
-    font-size: 36px;
+  ${Emoji} {
     &:hover {
-      font-size: 40px;
-    }
-    &:active {
-      opacity: 0;
+      font-size: 98px;
     }
   }
 `;
+
 function App() {
   return (
     <Wrapper>
       <Box>
-        <span>🥰</span>
+        <Emoji>🥰</Emoji>
       </Box>
     </Wrapper>
   );
